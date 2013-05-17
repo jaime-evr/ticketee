42c64582349f4ec1c56afdff8d47e0a96314eea5
jaime <jaime.victoria.90@gmail.com>
2013-05-16 20:28:40 -0500

    tickets form controller and model validatons

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,27 @@
+class TicketsController < ApplicationController
+  before_action :find_project
+
+  def new
+    @tickets = @project.tickets.build
+  end
+
+  def create
+    @ticket = @project.tickets.build(params[:ticket])
+    if @ticket.save
+      flash[:notice] = "Ticket has been created"
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "All fields required"
+      render action: "new"
+    end
+  end
+
+  private
+  def find_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def permited_params
+    params.require(:ticket).permit(:title, :description)
+  end
+end
